8be3d8f5ae35911429dd3c213f50839692060074
ninh.pham <manhninh91@gmail.com>
2021-04-07 18:14:08 +0700

    update folder schemas

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,14 +1,20 @@
 import AccessTokenSchema from "./AccessTokenSchema";
 import BlockSchema from "./BlockSchema";
 import ClientSchema from "./ClientSchema";
+import OrderSchema from "./OrderSchema";
 import RefreshTokenSchema from "./RefreshTokenSchema";
+import TradeHistorySchema from "./TradeHistorySchema";
 import UserSchema from "./UserSchema";
+import UserWalletSchema from "./UserWalletSchema";
 
 export {
   AccessTokenSchema,
   BlockSchema,
   ClientSchema,
   RefreshTokenSchema,
-  UserSchema
+  UserSchema,
+  UserWalletSchema,
+  OrderSchema,
+  TradeHistorySchema
 };
 
