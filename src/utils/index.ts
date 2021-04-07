ea25f4d25e30cf48f840ff271d35801abdd5e55d
ninh.pham <manhninh91@gmail.com>
2021-04-07 17:23:55 +0700

    export SendMailOptions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
+import { SendMailOptions } from 'nodemailer';
 import EmailConfig from './EmailConfig';
 import { errorMiddleware, notFoundMiddleware } from './Exceptions';
 import logger from './Logger';
 
-export { logger, errorMiddleware, notFoundMiddleware, EmailConfig };
+export { logger, errorMiddleware, notFoundMiddleware, EmailConfig, SendMailOptions };
 
