4367730e4e549a82dcbab1bf521ec6812ecef738
manhnguyenpg <manh.nguyentuan@powergatesoftware.com>
2021-04-24 21:31:04 +0700

    ref for collection

--- src/schemas/UserWalletSchema.ts
+++ src/schemas/UserWalletSchema.ts
@@ -1,19 +1,19 @@
-import mongoose, {Schema} from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 import IUserWalletModel from '../models/userWallets/IUserWalletModel';
 
 class UserWalletSchema {
   static get schema() {
     const schema = new mongoose.Schema(
       {
-        user_id: {type: Schema.Types.ObjectId, required: true},
-        amount: {type: Schema.Types.Number, required: true, default: 0},
-        amount_trade: {type: Schema.Types.Number, required: true, default: 0},
-        amount_demo: {type: Schema.Types.Number, required: true, default: 10000},
-        amount_expert: {type: Schema.Types.Number},
-        amount_copytrade: {type: Schema.Types.Number},
-        trc20: {type: Schema.Types.String, default: null},
-        erc20: {type: Schema.Types.String, default: null},
-        enable_withdraw: {type: Schema.Types.Boolean, default: true},
+        user_id: { type: Schema.Types.ObjectId, required: true, ref: 'users' },
+        amount: { type: Schema.Types.Number, required: true, default: 0 },
+        amount_trade: { type: Schema.Types.Number, required: true, default: 0 },
+        amount_demo: { type: Schema.Types.Number, required: true, default: 10000 },
+        amount_expert: { type: Schema.Types.Number },
+        amount_copytrade: { type: Schema.Types.Number },
+        trc20: { type: Schema.Types.String, default: null },
+        erc20: { type: Schema.Types.String, default: null },
+        enable_withdraw: { type: Schema.Types.Boolean, default: true },
       },
       {
         timestamps: true,
